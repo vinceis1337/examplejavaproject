228485f05692d45e2f595e71f9f484a8f88cff27
Vincent Liao <vliao@rmn.com>
2018-05-09 19:16:11 -0500

    Initial Commit

---
+++ src/Car.java
@@ -0,0 +1,50 @@
+public class Car {
+    public double value;
+    public int yearMade;
+    public String owner;
+    public String model;
+    public double depreciationTimeFactor;
+    public double depreciationMilesFactor;
+    public double milesPerGallon;
+
+    public Car(String model, String owner) {
+        this.model = model;
+        this.owner = owner;
+        this.yearMade = 2018;
+
+        switch (model) {
+            case "Cherokee":
+                this.value = 24395;
+                this.depreciationMilesFactor = 0.0000007;
+                this.depreciationTimeFactor = 0.04;
+                this.milesPerGallon = 18;
+                break;
+            case "Camry":
+                this.value = 23495;
+                this.depreciationMilesFactor = 0.0000002;
+                this.depreciationTimeFactor = 0.02;
+                this.milesPerGallon = 35;
+            case "Model S":
+                this.value = 74500;
+                this.depreciationMilesFactor = 0.0000002;
+                this.depreciationTimeFactor = 0.03;
+                this.milesPerGallon = 100;
+        }
+    }
+
+    public double valueAfter(int years, double milesDriven) {
+        //value * depreciationMilesFactor * milesDriven
+        double depreciatedMilesValue = value*depreciationMilesFactor*milesDriven;
+        //value x depreciationTimeFactor * years
+        double depreciatedTimeValue = value*depreciationTimeFactor*years;
+        //subtract both top from value initial.
+        double depreciatedValue = value - depreciatedMilesValue - depreciatedTimeValue;
+        return depreciatedValue;
+    }
+
+    public double costOfGas(float milesDriven) {
+        //costOfGas = milesDriven / milesPerGallon * gasPrice
+        double overallGasCost = milesDriven/milesPerGallon*2;
+        return overallGasCost;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println();
+        Car lanceCar = new Car("Cherokee", "Lance Champion"); // Lance drives his car until 2025 and puts 100000 miles on his car.
+        Car joeCar = new Car("Camry", "Joe Chen"); // Joe drives his car until 2040 and puts 400000 miles on his car.
+        Car markCar = new Car("Model S", "Mark Warbles"); // Mark drives his car until 2021 and puts 20000 miles on his car.
+
+        Car[] arrayOfCars = new Car[] { lanceCar, joeCar, markCar };
+
+
+        printStatement(lanceCar, 100000, 2025);
+
+        printStatement(joeCar, 400000, 2040);
+
+        printStatement(markCar, 20000, 2021);
+    }
+
+    public static void printStatement(Car car, int miles, int lastYearDriven) {
+        int carYears = lastYearDriven - car.yearMade;
+        double carDepreciatedValue = car.valueAfter(carYears, miles);
+        double carCostOfGas = car.costOfGas(miles);
+        double totalCarCost = car.value - carDepreciatedValue + carCostOfGas;
+        System.out.println(car.owner + "'s car is " + String.valueOf(carYears) + " years old.");
+        System.out.println("They have driven the car for " + String.valueOf(carYears) + " years and " + String.valueOf(miles) + " miles.");
+        System.out.println("The value of the car is now " + String.valueOf(carDepreciatedValue) + ".");
+        System.out.println("The cost of gasoline/electricity is " + String.valueOf(carCostOfGas) + ".");
+        System.out.println("The total cost of owning this vehicle (without insurance included) is " + String.valueOf(totalCarCost) + ".");
+        System.out.println();
+    }
+
+
+}
+
+/*
+    %NameOfDriver%'s car is X years old.
+    They have driven the car for X years and X miles.
+    The value of the car is now X.
+    The cost of gasoline/electricity is X.
+    The total cost of owning this vehicle (without insurance included) is X.
+ */
+
+public class CashValue {
+    double value;
+
+    public CashValue(double value) {
+        this.value = value;
+    }
+
+    public String formatValue() {
+        String.valueOf(Math.round(value * 100) / 100);
+    }
+}
